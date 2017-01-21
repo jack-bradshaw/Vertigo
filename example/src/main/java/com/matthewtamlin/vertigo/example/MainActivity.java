72c1497b386aaba3288df34c8e0fed0583ad1131
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-21 14:31:04 +1030

    Added Javadoc

--- example/src/main/java/com/matthewtamlin/vertigo/example/MainActivity.java
+++ example/src/main/java/com/matthewtamlin/vertigo/example/MainActivity.java
@@ -8,13 +8,28 @@ import com.matthewtamlin.vertigo.library.SimpleVertigoCoordinator;
 import com.matthewtamlin.vertigo.library.VertigoCoordinator;
 import com.matthewtamlin.vertigo.library.VertigoView;
 
+/**
+ * Displays three views as well as buttons for controlling the view positions.
+ */
 public class MainActivity extends AppCompatActivity {
+	/**
+	 * Key for referring to view 1 in the coordinator.
+	 */
 	private static final String VIEW_1_KEY = "view 1";
 
+	/**
+	 * Key for referring to view 2 in the coordinator.
+	 */
 	private static final String VIEW_2_KEY = "view 2";
 
+	/**
+	 * Key for referring to view 3 in the coordinator.
+	 */
 	private static final String VIEW_3_KEY = "view_3";
 
+	/**
+	 * Coordinates the views by sliding them up and down.
+	 */
 	private VertigoCoordinator coordinator;
 
 	@Override
@@ -24,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
 		setupCoordinationBehaviour();
 	}
 
+	/**
+	 * Sets up the three views in the coordinator.
+	 */
 	private void setupCoordinationBehaviour() {
 		coordinator = (SimpleVertigoCoordinator) findViewById(R.id.activity_main_coordinator);
 
@@ -40,14 +58,35 @@ public class MainActivity extends AppCompatActivity {
 		coordinator.registerViewForCoordination(view3, VIEW_3_KEY);
 	}
 
+	/**
+	 * On-click listener method for button 1. When called, view 1 is made active in the
+	 * coordinator.
+	 *
+	 * @param clickedView
+	 * 		the clicked view
+	 */
 	public void showView1(final View clickedView) {
 		coordinator.makeViewActive(VIEW_1_KEY, true, null);
 	}
 
+	/**
+	 * On-click listener method for button 2. When called, view 2 is made active in the
+	 * coordinator.
+	 *
+	 * @param clickedView
+	 * 		the clicked view
+	 */
 	public void showView2(final View clickedView) {
 		coordinator.makeViewActive(VIEW_2_KEY, true, null);
 	}
 
+	/**
+	 * On-click listener method for button 3. When called, view 3 is made active in the
+	 * coordinator.
+	 *
+	 * @param clickedView
+	 * 		the clicked view
+	 */
 	public void showView3(final View clickedView) {
 		coordinator.makeViewActive(VIEW_3_KEY, true, null);
 	}
